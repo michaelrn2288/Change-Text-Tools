4b7746cf309b72ad7aec393396bfe050cfa0cdc3
Michael Rocha Nunes <michaelrn2288@gmail.com>
2022-07-10 22:09:33 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const textArea = document.querySelector('#textArea');
+
+
+function allToUpper() {
+    const textEdited = textArea.value.toUpperCase();
+    textArea.value = textEdited;
+}
+
+
+function allToLower(){
+    const textEdited = textArea.value.toLowerCase();
+    textArea.value = textEdited;
+}
+
+
+function removeAllSpace() {
+    const textEdited = textArea.value.replace(/ /g,'');
+    textArea.value = textEdited;
+}
+
+function removeExcessSpace() {
+    while (textArea.value.includes('  ')) {
+        const textEdited = textArea.value.replace(/  /g,' ');
+        textArea.value = textEdited;
+    }
+}
+
+
+function firstToUpperEachWord() {
+    let textDivided = textArea.value.split(' ');
+    let wordsEdited = [];
+
+    for (let i = 0; i < textDivided.length; i++) {
+        wordsEdited[i] = textDivided[i].charAt(0).toUpperCase() + textDivided[i].slice(1);
+    }
+
+    const textEdited = wordsEdited.join(' ');
+    textArea.value = textEdited;
+}
+
+
+function firstToUpperEachPhrase() {
+    let textDivided = textArea.value.split('. ');
+    let phrasesEdited = [];
+
+    for (let i = 0; i < textDivided.length; i++) {
+        phrasesEdited[i] = textDivided[i].charAt(0).toUpperCase() + textDivided[i].slice(1);
+    }
+
+    const textEdited = phrasesEdited.join('. ');
+    textArea.value = textEdited;
+}
